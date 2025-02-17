11eb380365a6355954c46558af642b201f5bef72
Mohd Adnan Khan <mohdadnan02072002@gmail.com>
2025-02-17 12:46:05 +0530

    Scope with explicitly adding Singleton

--- 01-spring-boot-spring-core/08-Bean-Scope/src/main/java/com/visilean/commons/FootballCoach.java
+++ 01-spring-boot-spring-core/08-Bean-Scope/src/main/java/com/visilean/commons/FootballCoach.java
@@ -1,8 +1,11 @@
 package com.visilean.commons;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class FootballCoach implements Coach {
 
 	public String dailyWorkOut() {
