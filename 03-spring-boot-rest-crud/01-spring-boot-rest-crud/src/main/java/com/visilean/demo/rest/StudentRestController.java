51718c7ec9dfb94639e1f3b079ef857c694ea767
Mohd Adnan Khan <mohdadnan02072002@gmail.com>
2025-02-20 12:36:11 +0530

    REST POJO (Java POJO to JSON)

---
+++ 03-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/visilean/demo/rest/StudentRestController.java
@@ -0,0 +1,28 @@
+package com.visilean.demo.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.visilean.demo.entity.Student;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+	@GetMapping("/studentList")
+	public List<Student> getStudent() {
+
+		List<Student> stuList = new ArrayList<Student>();
+		stuList.add(new Student("Adnan", "Khan"));
+		stuList.add(new Student("Abhay", "Rajawat"));
+		stuList.add(new Student("Hasrh", "Tomar"));
+
+		return stuList;
+
+	}
+
+}
