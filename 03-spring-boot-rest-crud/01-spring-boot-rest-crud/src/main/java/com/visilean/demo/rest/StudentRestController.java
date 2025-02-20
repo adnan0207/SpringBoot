3e786d5ea36d6c00c6e0f45a8143d4c1ad58aae7
Mohd Adnan Khan <mohdadnan02072002@gmail.com>
2025-02-20 12:56:50 +0530

    Path Variable

--- 03-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/visilean/demo/rest/StudentRestController.java
+++ 03-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/visilean/demo/rest/StudentRestController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,19 @@ public class StudentRestController {
 		stuList.add(new Student("Hasrh", "Tomar"));
 
 		return stuList;
+
+	}
+
+	@GetMapping("/student/{studentId}")
+	public Student getStudentById(@PathVariable int studentId) {
+
+		List<Student> stuList = new ArrayList<Student>();
+
+		stuList.add(new Student("Adnan", "Khan"));
+		stuList.add(new Student("Abhay", "Rajawat"));
+		stuList.add(new Student("Harsh", "Tomar"));
+
+		return stuList.get(studentId);
 
 	}
 
