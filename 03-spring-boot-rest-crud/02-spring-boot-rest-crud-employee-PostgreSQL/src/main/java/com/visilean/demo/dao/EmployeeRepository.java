b278f18dc0f3e93bbf6d6993cedc67a2630ca427
Mohd Adnan Khan <mohdadnan02072002@gmail.com>
2025-02-24 14:35:28 +0530

    Specifing plural name/path with an annotation (@RepositoryRestResource(path=members))

--- 03-spring-boot-rest-crud/02-spring-boot-rest-crud-employee-PostgreSQL/src/main/java/com/visilean/demo/dao/EmployeeRepository.java
+++ 03-spring-boot-rest-crud/02-spring-boot-rest-crud-employee-PostgreSQL/src/main/java/com/visilean/demo/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.visilean.demo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.visilean.demo.entity.Employee;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
 
 }
