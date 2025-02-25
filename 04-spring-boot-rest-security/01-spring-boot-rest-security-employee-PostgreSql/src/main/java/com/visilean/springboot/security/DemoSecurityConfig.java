24e67325fd0d20917c3c3bc1411b381849cd0342
Mohd Adnan Khan <mohdadnan02072002@gmail.com>
2025-02-25 15:18:47 +0530

    Spring Security with user account stored in Database

--- 04-spring-boot-rest-security/01-spring-boot-rest-security-employee-PostgreSql/src/main/java/com/visilean/springboot/security/DemoSecurityConfig.java
+++ 04-spring-boot-rest-security/01-spring-boot-rest-security-employee-PostgreSql/src/main/java/com/visilean/springboot/security/DemoSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.visilean.springboot.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,6 +10,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,40 +19,49 @@ public class DemoSecurityConfig {
 	
 	// since we have defined the users here spring boot will not use the user/pass from the application.properties file
 
+//	@Bean
+//	public InMemoryUserDetailsManager userDetailsManager() {
+//
+//		UserDetails harsh = User.builder()
+//				.username("harsh")
+//				.password("{noop}test123")
+//				.roles("EMPLOYEE")
+//				.build();
+//
+//		UserDetails abhay = User.builder()
+//				.username("abhay")
+//				.password("{noop}test123")
+//				.roles("EMPLOYEE", "MANAGER")
+//				.build();
+//
+//		UserDetails adnan = User.builder()
+//				.username("adnan")
+//				.password("{noop}test123")
+//				.roles("EMPLOYEE", "MANAGER", "ADMIN")
+//				.build();
+//		
+//		return new InMemoryUserDetailsManager(harsh, abhay, adnan);
+//	}
+
+	// add support for JDBC ......... no more hardcoded users
+	
 	@Bean
-	public InMemoryUserDetailsManager userDetailsManager() {
-
-		UserDetails harsh = User.builder()
-				.username("harsh")
-				.password("{noop}test123")
-				.roles("EMPLOYEE")
-				.build();
-
-		UserDetails abhay = User.builder()
-				.username("abhay")
-				.password("{noop}test123")
-				.roles("EMPLOYEE", "MANAGER")
-				.build();
-
-		UserDetails adnan = User.builder()
-				.username("adnan")
-				.password("{noop}test123")
-				.roles("EMPLOYEE", "MANAGER", "ADMIN")
-				.build();
-		
-		return new InMemoryUserDetailsManager(harsh, abhay, adnan);
+	public UserDetailsManager userDetailsManager(DataSource dataSource) {		//  Inject data source Auto-configured by Spring Boot
+		return new JdbcUserDetailsManager(dataSource);			
+		// JdbcUserDetailsManager -> Tell Spring Security to use JDBC authentication with our data source
+		// dataSource -> no longer hard coding users
 	}
-
+	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		
 		http.authorizeHttpRequests(configurer -> 
 			configurer
-				.requestMatchers(HttpMethod.GET, "api/employees").hasRole("EMPLOYEE")
-				.requestMatchers(HttpMethod.GET, "api/employees/**").hasRole("EMPLOYEE")
-				.requestMatchers(HttpMethod.POST, "api/employees").hasRole("MANAGER")
-				.requestMatchers(HttpMethod.PUT, "api/employees").hasRole("MANAGER")
-				.requestMatchers(HttpMethod.DELETE, "api/employees/**").hasRole("ADMIN")
+				.requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+				.requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+				.requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+				.requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+				.requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
 				
 		);
 		
@@ -66,3 +79,41 @@ public class DemoSecurityConfig {
 	}
 	
 }
+
+
+// db script
+
+//-- Drop tables if they exist
+//DROP TABLE IF EXISTS authorities;
+//DROP TABLE IF EXISTS users;
+//
+//-- Table structure for table `users`
+//CREATE TABLE users (
+//  username VARCHAR(50) NOT NULL,
+//  password VARCHAR(50) NOT NULL,
+//  enabled SMALLINT NOT NULL,
+//  PRIMARY KEY (username)
+//);
+//
+//-- Inserting data for table `users`
+//INSERT INTO users (username, password, enabled) VALUES 
+//('harsh', '{noop}test123', 1),
+//('abhay', '{noop}test123', 1),
+//('adnan', '{noop}test123', 1);
+//
+//-- Table structure for table `authorities`
+//CREATE TABLE authorities (
+//  username VARCHAR(50) NOT NULL,
+//  authority VARCHAR(50) NOT NULL,
+//  UNIQUE (username, authority),
+//  FOREIGN KEY (username) REFERENCES users (username)
+//);
+//
+//-- Inserting data for table `authorities`
+//INSERT INTO authorities (username, authority) VALUES 
+//('harsh', 'ROLE_EMPLOYEE'),
+//('abhay', 'ROLE_EMPLOYEE'),
+//('abhay', 'ROLE_MANAGER'),
+//('adnan', 'ROLE_EMPLOYEE'),
+//('adnan', 'ROLE_MANAGER'),
+//('adnan', 'ROLE_ADMIN');
--- 04-spring-boot-rest-security/01-spring-boot-rest-security-employee/src/main/java/com/visilean/springboot/security/DemoSecurityConfig.java
+++ 04-spring-boot-rest-security/01-spring-boot-rest-security-employee/src/main/java/com/visilean/springboot/security/DemoSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.visilean.springboot.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,6 +10,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,28 +19,37 @@ public class DemoSecurityConfig {
 	
 	// since we have defined the users here spring boot will not use the user/pass from the application.properties file
 
+//	@Bean
+//	public InMemoryUserDetailsManager userDetailsManager() {
+//
+//		UserDetails harsh = User.builder()
+//				.username("harsh")
+//				.password("{noop}test123")
+//				.roles("EMPLOYEE")
+//				.build();
+//
+//		UserDetails abhay = User.builder()
+//				.username("abhay")
+//				.password("{noop}test123")
+//				.roles("EMPLOYEE", "MANAGER")
+//				.build();
+//
+//		UserDetails adnan = User.builder()
+//				.username("adnan")
+//				.password("{noop}test123")
+//				.roles("EMPLOYEE", "MANAGER", "ADMIN")
+//				.build();
+//		
+//		return new InMemoryUserDetailsManager(harsh, abhay, adnan);
+//	}
+	
+	// add support for JDBC ......... no more hardcoded users
+	
 	@Bean
-	public InMemoryUserDetailsManager userDetailsManager() {
-
-		UserDetails harsh = User.builder()
-				.username("harsh")
-				.password("{noop}test123")
-				.roles("EMPLOYEE")
-				.build();
-
-		UserDetails abhay = User.builder()
-				.username("abhay")
-				.password("{noop}test123")
-				.roles("EMPLOYEE", "MANAGER")
-				.build();
-
-		UserDetails adnan = User.builder()
-				.username("adnan")
-				.password("{noop}test123")
-				.roles("EMPLOYEE", "MANAGER", "ADMIN")
-				.build();
-		
-		return new InMemoryUserDetailsManager(harsh, abhay, adnan);
+	public UserDetailsManager userDetailsManager(DataSource dataSource) {		//  Inject data source Auto-configured by Spring Boot
+		return new JdbcUserDetailsManager(dataSource);			
+		// JdbcUserDetailsManager -> Tell Spring Security to use JDBC authentication with our data source
+		// dataSource -> no longer hard coding users
 	}
 	
 	@Bean
@@ -44,11 +57,11 @@ public class DemoSecurityConfig {
 		
 		http.authorizeHttpRequests(configurer -> 
 			configurer
-				.requestMatchers(HttpMethod.GET, "api/employees").hasRole("EMPLOYEE")
-				.requestMatchers(HttpMethod.GET, "api/employees/**").hasRole("EMPLOYEE")
-				.requestMatchers(HttpMethod.POST, "api/employees").hasRole("MANAGER")
-				.requestMatchers(HttpMethod.PUT, "api/employees").hasRole("MANAGER")
-				.requestMatchers(HttpMethod.DELETE, "api/employees/**").hasRole("ADMIN")
+				.requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+				.requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+				.requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+				.requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+				.requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
 				
 		);
 		
@@ -66,3 +79,57 @@ public class DemoSecurityConfig {
 	}
 	
 }
+
+
+// db script
+
+//USE `employee_directory`;
+//
+//DROP TABLE IF EXISTS `authorities`;
+//DROP TABLE IF EXISTS `users`;
+//
+//--
+//-- Table structure for table `users`
+//--
+//
+//CREATE TABLE `users` (
+//  `username` varchar(50) NOT NULL,
+//  `password` varchar(50) NOT NULL,
+//  `enabled` tinyint NOT NULL,
+//  PRIMARY KEY (`username`)
+//) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+//
+//--
+//-- Inserting data for table `users`
+//--
+//
+//INSERT INTO `users` 
+//VALUES 
+//('harsh','{noop}test123',1),
+//('abhay','{noop}test123',1),
+//('adnan','{noop}test123',1);
+//
+//
+//--
+//-- Table structure for table `authorities`
+//--
+//
+//CREATE TABLE `authorities` (
+//  `username` varchar(50) NOT NULL,
+//  `authority` varchar(50) NOT NULL,
+//  UNIQUE KEY `authorities_idx_1` (`username`,`authority`),
+//  CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+//) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+//
+//--
+//-- Inserting data for table `authorities`
+//--
+//
+//INSERT INTO `authorities` 
+//VALUES 
+//('harsh','ROLE_EMPLOYEE'),
+//('abhay','ROLE_EMPLOYEE'),
+//('abhay','ROLE_MANAGER'),
+//('adnan','ROLE_EMPLOYEE'),
+//('adnan','ROLE_MANAGER'),
+//('adnan','ROLE_ADMIN');
