0108153397d3479b00f3846ce7fbad1b60e9df89
Mohd Adnan Khan <mohdadnan02072002@gmail.com>
2025-02-27 16:23:38 +0530

    adding data to mvc model comming from the form

--- 05-spring-boot-spring-mvc/01-thymeleaf-helloworld/src/main/java/com/visilean/springboot/controller/HelloFormController.java
+++ 05-spring-boot-spring-mvc/01-thymeleaf-helloworld/src/main/java/com/visilean/springboot/controller/HelloFormController.java
@@ -1,7 +1,10 @@
 package com.visilean.springboot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloFormController {
@@ -18,5 +21,26 @@ public class HelloFormController {
 	public String processForm() {
 		return "nameOutputForm";
 	}
+	
+	
+	// need a controller method to read the form data and add that data to the model
+	@RequestMapping("/processFormVersionTwo")
+	public String letsShout(HttpServletRequest req, Model model) {
+		// read the request parameter from the HTML form
+		String theName = req.getParameter("name");
+		
+		// convert data to all caps
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result = "Yo! " + theName;
+		
+		// add the message to model
+		model.addAttribute("message", result);
+		
+		return "nameOutputVersionTwo";
+	}
+	
+	
 	
 }
