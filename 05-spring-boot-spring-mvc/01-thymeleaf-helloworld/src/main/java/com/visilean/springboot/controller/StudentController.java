bc8a84a0808990b8559d4924f73b44807c0de140
Mohd Adnan Khan <mohdadnan02072002@gmail.com>
2025-02-28 12:53:25 +0530

    Form data binding - text fields

---
+++ 05-spring-boot-spring-mvc/01-thymeleaf-helloworld/src/main/java/com/visilean/springboot/controller/StudentController.java
@@ -0,0 +1,35 @@
+package com.visilean.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.visilean.springboot.model.Student;
+
+@Controller
+public class StudentController {
+
+	@GetMapping("/showStudentForm")
+	public String showForm(Model model) {
+		
+		// create student object
+		Student stuObj = new Student();
+		
+		// adding student object to model
+		model.addAttribute("student", stuObj);
+		
+		return "student-form";
+	}
+	
+	@PostMapping("/processStudentForm")
+	public String processForm(@ModelAttribute("student") Student theStudent) {
+		
+		// log the input data
+		System.out.println("The student : " + theStudent.getFirstName() + " " + theStudent.getLastName());
+		
+		return "student-confirmation";
+		
+	}
+}
