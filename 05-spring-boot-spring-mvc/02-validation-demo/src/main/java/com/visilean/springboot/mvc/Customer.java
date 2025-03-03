a20070c625e8110c21f843384afb3fa98b40f030
Mohd Adnan Khan <mohdadnan02072002@gmail.com>
2025-03-03 15:20:23 +0530

    Validation Regular Expression

--- 05-spring-boot-spring-mvc/02-validation-demo/src/main/java/com/visilean/springboot/mvc/Customer.java
+++ 05-spring-boot-spring-mvc/02-validation-demo/src/main/java/com/visilean/springboot/mvc/Customer.java
@@ -3,6 +3,7 @@ package com.visilean.springboot.mvc;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class Customer {
@@ -16,6 +17,9 @@ public class Customer {
 	@Min(value = 0, message = "must be greater than or equal to Zero")
 	@Max(value = 10, message = "must be smaller than or equal to Ten")
 	private int freePasses;
+	
+	@Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "Only 5 digits/characters")
+	private String postalCode;
 	
 	
 	public String getFirstName() {
@@ -40,5 +44,13 @@ public class Customer {
 	
 	public void setFreePasses(int freePasses) {
 		this.freePasses = freePasses;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
 	}
 }
