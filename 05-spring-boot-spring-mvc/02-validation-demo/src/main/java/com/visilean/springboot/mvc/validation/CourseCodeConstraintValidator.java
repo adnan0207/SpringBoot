d14f0c0d373e2d0cf0d2fc75a941721f3c216dfc
Mohd Adnan Khan <mohdadnan02072002@gmail.com>
2025-03-04 14:33:47 +0530

    fixed null input in the course code section issue

--- 05-spring-boot-spring-mvc/02-validation-demo/src/main/java/com/visilean/springboot/mvc/validation/CourseCodeConstraintValidator.java
+++ 05-spring-boot-spring-mvc/02-validation-demo/src/main/java/com/visilean/springboot/mvc/validation/CourseCodeConstraintValidator.java
@@ -11,12 +11,18 @@ public class CourseCodeConstraintValidator implements ConstraintValidator<Course
 		coursePrefix = theCourseCode.value();
 	}
 
-
 	// spring mvc will call isValid
 	public boolean isValid(String theCode, ConstraintValidatorContext theConstraintValidatorContext) {
-		boolean result = theCode.startsWith(coursePrefix);
+		boolean result;
+		
+		if(theCode != null) {
+			result = theCode.startsWith(coursePrefix);
+		}else {
+			result = true;
+		}
+		
 		return result;
+
 	}
-	
 
 }
