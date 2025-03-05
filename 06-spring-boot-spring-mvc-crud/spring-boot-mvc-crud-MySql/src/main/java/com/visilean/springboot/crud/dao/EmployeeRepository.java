905d661ae618e844d80c1e52a587327113a6fd77
Mohd Adnan Khan <mohdadnan02072002@gmail.com>
2025-03-05 14:50:11 +0530

    Add Employee

--- 06-spring-boot-spring-mvc-crud/spring-boot-mvc-crud-MySql/src/main/java/com/visilean/springboot/crud/dao/EmployeeRepository.java
+++ 06-spring-boot-spring-mvc-crud/spring-boot-mvc-crud-MySql/src/main/java/com/visilean/springboot/crud/dao/EmployeeRepository.java
@@ -1,9 +1,14 @@
 package com.visilean.springboot.crud.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.visilean.springboot.crud.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+	
+	// add a method to sort by first name
+	public List<Employee> findAllByOrderByFirstNameAsc();
 
 }
--- 06-spring-boot-spring-mvc-crud/spring-boot-mvc-crud-PostgreSQL/src/main/java/com/visilean/springboot/crud/controller/EmployeeController.java
+++ 06-spring-boot-spring-mvc-crud/spring-boot-mvc-crud-PostgreSQL/src/main/java/com/visilean/springboot/crud/controller/EmployeeController.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.visilean.springboot.crud.entity.Employee;
 import com.visilean.springboot.crud.service.EmployeeService;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 @RequestMapping("/employees")
@@ -31,7 +34,32 @@ public class EmployeeController {
 		
 		theModel.addAttribute("employees", theEmployees);
 		
-		return "list-employees";
+		return "employees/list-employees";
 	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		// create model attribute to bind the data
+		
+		Employee theEmployee = new Employee();
+		
+		theModel.addAttribute("employee", theEmployee);
+		
+		return "employees/employee-form";
+	}
+	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+		
+		// save the employee
+		
+		employeeService.save(theEmployee);
+		
+		// use a redirect to prevent duplicate submission
+		
+		return "redirect:/employees/list";
+	}
+	
 
 }
