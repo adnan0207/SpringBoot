cb97780970dde60c0d0d55135b7cad9d4c6602a3
Mohd Adnan Khan <mohdadnan02072002@gmail.com>
2025-03-17 13:37:40 +0530

    Update Student for many to many

--- 08-spring-boot-jpa-advanced-mappings/03-jpa-many-to-many-PostgreSQL/src/main/java/com/visilean/cruddemo/entity/Student.java
+++ 08-spring-boot-jpa-advanced-mappings/03-jpa-many-to-many-PostgreSQL/src/main/java/com/visilean/cruddemo/entity/Student.java
@@ -1,10 +1,16 @@
 package com.visilean.cruddemo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -24,6 +30,9 @@ public class Student {
 
 	@Column(name = "email")
 	private String email;
+	
+	@ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}, mappedBy = "students")
+	private List<Course> courses;
 
 	public Student() {
 	}
@@ -64,6 +73,27 @@ public class Student {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Course> getCourses() {
+		return courses;
+	}
+	
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+	
+	// adding a convenience method
+	
+	public void addCourse(Course theCourse) {
+		
+		if(courses == null) {
+			courses = new ArrayList<Course>();
+		}
+		
+		courses.add(theCourse);
+		theCourse.addStudent(this);
+		
 	}
 
 	@Override
