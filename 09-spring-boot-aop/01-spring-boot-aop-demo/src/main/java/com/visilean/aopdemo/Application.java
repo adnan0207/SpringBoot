bfbd479ac39cb8327bbf52d59bcee2c5dcfc5e95
Mohd Adnan Khan <mohdadnan02072002@gmail.com>
2025-03-20 12:00:59 +0530

    @Before Advice

--- 09-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/visilean/aopdemo/Application.java
+++ 09-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/visilean/aopdemo/Application.java
@@ -1,13 +1,34 @@
 package com.visilean.aopdemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.visilean.aopdemo.dao.AccountDAO;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO) {
+
+		return runner -> {
+			demoTheBeforeAdvice(theAccountDAO);
+		};
+
+	}
+
+	private void demoTheBeforeAdvice(AccountDAO theAccountDAO) {
+
+		// call the business model
+		theAccountDAO.addAccount();
+		System.out.println("\n Calling it again");
+		theAccountDAO.addAccount();
 	}
 
 }
---
+++ 09-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/visilean/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,7 @@
+package com.visilean.aopdemo.dao;
+
+public interface AccountDAO {
+	
+	public void addAccount();
+
+}
