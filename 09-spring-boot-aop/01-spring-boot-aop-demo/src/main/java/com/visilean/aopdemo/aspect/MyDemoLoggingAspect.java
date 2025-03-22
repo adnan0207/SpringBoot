31dedcf9e3594a3b25a901f3f5147f1a2a58973c
Mohd Adnan Khan <mohdadnan02072002@gmail.com>
2025-03-22 20:10:34 +0530

    Match method in DAO package and exclude getter/setter methods

--- 09-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/visilean/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 09-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/visilean/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -24,12 +24,27 @@ public class MyDemoLoggingAspect {
 	private void forDaoPackage() {
 	}
 	
-	@Before("forDaoPackage()")
+	// create a pointcut for getter method
+	@Pointcut("execution(public * com.visilean.aopdemo.dao.*.get*(..))")
+	private void forGetters() {
+	}
+	
+	// create a pointcut for setter method
+	@Pointcut("execution(public * com.visilean.aopdemo.dao.*.set*(..))")
+	private void forSetters() {
+	}
+	
+	// create a pointcut to include the package and exclude the getter/setter
+	@Pointcut("forDaoPackage() && !(forGetters() || forSetters() )")
+	private void forDaoPackageNoGetterSetter() {
+	}
+	
+	@Before("forDaoPackageNoGetterSetter()")
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n ====> Executing @Before advice on method");
 	}
 	
-	@Before("forDaoPackage()")
+	@Before("forDaoPackageNoGetterSetter()")
 	public void performApiAnalytics() {
 		System.out.println(" ====> performing API analytics");
 	}
