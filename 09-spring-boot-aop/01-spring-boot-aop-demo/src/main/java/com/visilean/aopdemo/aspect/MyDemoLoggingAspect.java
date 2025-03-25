51eb91835f4dc1d04821b2b8d0112c851143bb0a
Mohd Adnan Khan <mohdadnan02072002@gmail.com>
2025-03-25 16:45:28 +0530

    @AfterThrowing Advice

--- 09-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/visilean/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 09-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/visilean/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
@@ -17,7 +18,16 @@ import com.visilean.aopdemo.entity.Account;
 @Order(2)
 public class MyDemoLoggingAspect {
 	
-	// adding a new advice for @AfterReturning on the findAccounts method
+	@AfterThrowing(pointcut = "execution(* com.visilean.aopdemo.dao.AccountDAO.findAccounts(..))", throwing = "theExc")
+	public void afterThrowingFindAccountsAdvice(JoinPoint theJoinPoint, Throwable theExc) {
+		
+		// print out which method we are advising on
+		String method = theJoinPoint.getSignature().toShortString();
+		System.out.println("\n ===> Executing @AfterThrowing on method : " + method);
+		
+		// log the exception 
+		System.out.println("\n ===> The exception : " + theExc);
+	}
 	
 	@AfterReturning(pointcut = "execution(* com.visilean.aopdemo.dao.AccountDAO.findAccounts(..))", returning = "result")
 	public void afterReturningFindAccountsAdvice(JoinPoint theJoinPoint, List<Account> result) {
