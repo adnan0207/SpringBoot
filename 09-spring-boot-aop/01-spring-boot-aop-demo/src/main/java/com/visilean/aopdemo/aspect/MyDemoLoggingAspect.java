56d4cc1f1bcb7c75a135fc6d6580ab20b718bce3
Mohd Adnan Khan <mohdadnan02072002@gmail.com>
2025-03-25 15:24:52 +0530

    AOP code to modify the data return from the method

--- 09-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/visilean/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 09-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/visilean/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,6 +1,9 @@
 package com.visilean.aopdemo.aspect;
 
+import java.util.List;
+
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
@@ -13,6 +16,37 @@ import com.visilean.aopdemo.entity.Account;
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
+	
+	// adding a new advice for @AfterReturning on the findAccounts method
+	
+	@AfterReturning(pointcut = "execution(* com.visilean.aopdemo.dao.AccountDAO.findAccounts(..))", returning = "result")
+	public void afterReturningFindAccountsAdvice(JoinPoint theJoinPoint, List<Account> result) {
+		
+		// print out which method we are advising on
+		String method = theJoinPoint.getSignature().toShortString();
+		System.out.println("\n ===> Executing @AfterReturning on method : " + method);
+		
+		// print out the result of the method call
+		System.out.println("\n ===> The result : " + result);
+		
+		// let's post-process the data... let's modify it
+		// convert the account names to upper case
+		convertAccountNamesToUpperCase(result);
+
+		System.out.println("\n ===> The result : " + result);
+	}
+
+	private void convertAccountNamesToUpperCase(List<Account> result) {
+		// loop through the accounts
+		for(Account acc : result) {
+			// get upper case versions of name
+			String upperName = acc.getName().toUpperCase();
+
+			// update the name on the account
+			acc.setName(upperName);
+		}
+		
+	}
 
 	@Before("com.visilean.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
 	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
